a872508a33c05eb0075117eac64ccef7f5ac87a2
bhargav-devops-7 <bhargavdodda996@gmail.com>
2026-03-16 16:25:18 +0530

    terraform-ec2-test

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 module "example" {
     source = "git::https://github.com/bhargav-devops-7/terraform-aws-instance.git?ref=main"
 
-    # ami_id = data.aws_ami.joindevops.id
-    ami_id = var.ami_id
+    ami_id = data.aws_ami.joindevops.id
+    # ami_id = var.ami_id
     instance_type = var.instance_type
     sg_ids = var.sg_ids
     tags = var.tags
